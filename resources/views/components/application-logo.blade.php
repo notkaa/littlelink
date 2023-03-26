9a4dbcc0448cf9d0338a3ea7c8689b18d8b7009d
notkaa <55271235+notkaa@users.noreply.github.com>
2023-03-26 11:28:39 +0200

    back to littlelink

--- resources/views/components/application-logo.blade.php
+++
@@ -1,5 +0,0 @@
-@if(file_exists(base_path("/littlelink/images/avatar.png" )))
-    <img class="mb-5" src="{{ asset('/littlelink/images/avatar.png') }}"  style="width: 150px;">
-@else
-    <img class="mb-5" src="{{ asset('/littlelink/images/avatar@2x.png') }}">
-@endif
--- resources/views/components/pageitems/heading-form.blade.php
+++
@@ -1,4 +0,0 @@
-<label for='title' class='form-label'>Heading Text:</label>
-<input type='text' name='title' value='{{$link_title}}' class='form-control' />
-
-
